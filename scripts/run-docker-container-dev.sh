ee19d973ac6d7c015a50751889816a011ec7fe7b
Vũ Đức Thái <vuducthaibka62@gmail.com>
2026-03-11 17:52:10 +0700

    Modified database migration scripts

--- scripts/run-docker-container-dev.sh
+++ scripts/run-docker-container-dev.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-IMAGE_NAME=fiagram_account_service
+IMAGE_NAME=fiagram_standalone
 IMAGE_VERSION=$(cat ./VERSION)
 
 docker run --rm -it \
-    -v "$PWD/deployments/configs:/home:ro" \
+    -v "$PWD/deployments/configs/test.yaml:/home/test.yaml:ro" \
     -p 11000:8080 \
     $IMAGE_NAME:$IMAGE_VERSION \
-    /account_service -c /home/test.yaml
+    /standalone -c /home/test.yaml
